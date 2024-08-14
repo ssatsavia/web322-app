fc0fc9b6092b54960afdc1d2f3d5007dab9e287a
ssatsavia <145367186+ssatsavia@users.noreply.github.com>
2024-08-14 13:22:48 -0400

    vercel fixes

--- auth-service.js
+++ auth-service.js
@@ -1,87 +1,89 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+let Schema = mongoose.Schema;
 
+const userSchema = new Schema({
+    userName: { type: String, unique: true },
+    password: String,
+    email: String,
+    loginHistory: [{
+        dateTime: Date,
+        userAgent: String
+    }]
+});
 
-        const mongoose = require('mongoose');
-        let Schema = mongoose.Schema;
-        const bcrypt = require('bcryptjs');
+let User; // To be defined on new connection
+
+// Initialize the module.exports object
+module.exports = {};
+
+// Define the initialize method
+module.exports.initialize = function () {
+    return new Promise((resolve, reject) => {
+        let db = mongoose.createConnection("mongodb+srv://sagesatsavia:xgzEDbjWVh3o1OR0@web322.xwyr4.mongodb.net/");
         
-        const userSchema = new Schema({
-            userName: { type: String, unique: true },
-            password: String,
-            email: String,
-            loginHistory: [{
-                dateTime: Date,
-                userAgent: String
-            }]
+        db.on('error', (err) => {
+            reject(err);
         });
-        
-        let User; // To be defined on new connection
-        
-        module.exports = User; 
-        
-        module.exports.initialize = function () {
-            return new Promise((resolve, reject) => {
-                let db = mongoose.createConnection("mongodb+srv://sagesatsavia:xgzEDbjWVh3o1OR0@web322.xwyr4.mongodb.net/"); // check this once again before submitting 
-        
-                db.on('error', (err) => {
-                    reject(err);
-                });
-        
-                db.once('open', () => {
-                    User = db.model("users", userSchema);
-                    resolve();
-                });
-            });
-        };
-        
-        module.exports.registerUser = function (userData) {
-            return new Promise((resolve, reject) => {
-                if (userData.password !== userData.password2) {
-                    reject("Passwords do not match");
-                } else {
-                    bcrypt.hash(userData.password, 10).then((hash) => {
-                        userData.password = hash;
-                        let newUser = new User(userData);
-                        newUser.save()
-                            .then(() => resolve())
-                            .catch((err) => {
-                                if (err.code === 11000) { // Handle unique constraint error (duplicate userName)
-                                    reject("User Name already taken");
-                                } else {
-                                    reject(`There was an error creating the user: ${err}`);
-                                }
-                            });
-                    }).catch(err => reject("There was an error encrypting the password"));
-                }
-            });
-        };
-        
-        module.exports.checkUser = function (userData) {
-            return new Promise((resolve, reject) => {
-                User.findOne({ userName: userData.userName }).exec()
-                    .then(user => {
-                        if (!user) {
-                            reject(`Unable to find user: ${userData.userName}`);
+
+        db.once('open', () => {
+            User = db.model("users", userSchema);
+            resolve();
+        });
+    });
+};
+
+// Define the registerUser method
+module.exports.registerUser = function (userData) {
+    return new Promise((resolve, reject) => {
+        if (userData.password !== userData.password2) {
+            reject("Passwords do not match");
+        } else {
+            bcrypt.hash(userData.password, 10).then((hash) => {
+                userData.password = hash;
+                let newUser = new User(userData);
+                newUser.save()
+                    .then(() => resolve())
+                    .catch((err) => {
+                        if (err.code === 11000) { // Handle unique constraint error (duplicate userName)
+                            reject("User Name already taken");
                         } else {
-                            bcrypt.compare(userData.password, user.password).then(result => {
-                                if (result) {
-                                    user.loginHistory.push({
-                                        dateTime: new Date(),
-                                        userAgent: userData.userAgent
-                                    });
-        
-                                    User.updateOne(
-                                        { userName: user.userName },
-                                        { $set: { loginHistory: user.loginHistory } }
-                                    ).exec().then(() => {
-                                        resolve(user);
-                                    }).catch(err => reject(`There was an error verifying the user: ${err}`));
-                                } else {
-                                    reject(`Incorrect Password for user: ${userData.userName}`);
-                                }
-                            });
+                            reject(`There was an error creating the user: ${err}`);
                         }
-                    }).catch(err => {
-                        reject(`Unable to find user: ${userData.userName}`);
                     });
+            }).catch(err => reject("There was an error encrypting the password"));
+        }
+    });
+};
+
+// Define the checkUser method
+module.exports.checkUser = function (userData) {
+    return new Promise((resolve, reject) => {
+        User.findOne({ userName: userData.userName }).exec()
+            .then(user => {
+                if (!user) {
+                    reject(`Unable to find user: ${userData.userName}`);
+                } else {
+                    bcrypt.compare(userData.password, user.password).then(result => {
+                        if (result) {
+                            user.loginHistory.push({
+                                dateTime: new Date(),
+                                userAgent: userData.userAgent
+                            });
+
+                            User.updateOne(
+                                { userName: user.userName },
+                                { $set: { loginHistory: user.loginHistory } }
+                            ).exec().then(() => {
+                                resolve(user);
+                            }).catch(err => reject(`There was an error verifying the user: ${err}`));
+                        } else {
+                            reject(`Incorrect Password for user: ${userData.userName}`);
+                        }
+                    });
+                }
+            }).catch(err => {
+                reject(`Unable to find user: ${userData.userName}`);
             });
-        };
+    });
+};
